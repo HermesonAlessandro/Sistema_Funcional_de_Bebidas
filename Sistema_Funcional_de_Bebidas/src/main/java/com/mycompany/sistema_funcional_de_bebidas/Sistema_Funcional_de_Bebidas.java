1fa8a6a55d11fe77ae7f46aea69af782295865e1
@HermesonAlessandro <105324312+HermesonAlessandro@users.noreply.github.com>
2024-08-25 18:56:39 -0300

    Criação do Repositório e do Projeto
    Criação do Repositório e do Projeto

--- Sistema_Funcional_de_Bebidas/src/main/java/com/mycompany/sistema_funcional_de_bebidas/Sistema_Funcional_de_Bebidas.java
+++ Sistema_Funcional_de_Bebidas/src/main/java/com/mycompany/sistema_funcional_de_bebidas/Sistema_Funcional_de_Bebidas.java
@@ -12,5 +12,9 @@ public class Sistema_Funcional_de_Bebidas {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        ddfd
+                
+                dfdfd
+                
     }
 }
