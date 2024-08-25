09abe414ba1feccc402c447bac05afbb23f485bd
@HermesonAlessandro <105324312+HermesonAlessandro@users.noreply.github.com>
2024-08-25 18:41:05 -0300

    Criação do Projeto
    Criação do Projeto

---
+++ Sistema_Funcional_de_Bebidas/src/main/java/Sistema_Funcional_de_Bebidas/Sistema_Funcional_de_Bebidas.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package Sistema_Funcional_de_Bebidas;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Hermeson Alessandro
+ */
+public class Sistema_Funcional_de_Bebidas {
+
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "ola mundo");
+    }
+}
