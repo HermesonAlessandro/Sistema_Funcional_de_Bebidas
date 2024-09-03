8bdff591cfd9f3dfa72f173cd2ea3a97b7adf196
@HermesonAlessandro <105324312+HermesonAlessandro@users.noreply.github.com>
2024-09-03 02:11:22 -0300

    Ajustes
    Ajustes

--- Sistema_Funcional_de_Bebidas/src/main/java/Visao/Tela_inicial_administrador.java
+++ Sistema_Funcional_de_Bebidas/src/main/java/Visao/Tela_inicial_administrador.java
@@ -32,11 +32,11 @@ public class Tela_inicial_administrador extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGap(0, 652, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGap(0, 517, Short.MAX_VALUE)
         );
 
         pack();
