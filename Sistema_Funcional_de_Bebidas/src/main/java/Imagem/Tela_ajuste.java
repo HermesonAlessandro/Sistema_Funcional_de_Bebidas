cfb5f33ebcd2ac5c466c2c9149cc2fbde66a99f2
@HermesonAlessandro <105324312+HermesonAlessandro@users.noreply.github.com>
2024-11-12 23:00:39 -0300

    Ajustes!
    Ajustes!

--- Sistema_Funcional_de_Bebidas/src/main/java/Imagem/Tela_ajuste.java
+++ Sistema_Funcional_de_Bebidas/src/main/java/Imagem/Tela_ajuste.java
@@ -2,18 +2,18 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
-package Imagem; // Tela para ajustes apenas!
+package Imagem;
 
 /**
  *
  * @author Hermeson Alessandro
  */
-public class Tela_ajuste_telas extends javax.swing.JFrame {
+public class Tela_ajuste extends javax.swing.JFrame {
 
     /**
-     * Creates new form Tela_ajuste_telas
+     * Creates new form Tela_ajuste
      */
-    public Tela_ajuste_telas() {
+    public Tela_ajuste() {
         initComponents();
     }
 
@@ -32,11 +32,11 @@ public class Tela_ajuste_telas extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 461, Short.MAX_VALUE)
+            .addGap(0, 450, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 311, Short.MAX_VALUE)
+            .addGap(0, 374, Short.MAX_VALUE)
         );
 
         pack();
@@ -59,20 +59,20 @@ public class Tela_ajuste_telas extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Tela_ajuste_telas.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Tela_ajuste.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Tela_ajuste_telas.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Tela_ajuste.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Tela_ajuste_telas.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Tela_ajuste.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Tela_ajuste_telas.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Tela_ajuste.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new Tela_ajuste_telas().setVisible(true);
+                new Tela_ajuste().setVisible(true);
             }
         });
     }
