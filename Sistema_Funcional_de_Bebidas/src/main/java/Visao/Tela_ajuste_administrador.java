8340009ba8f48d0e016ec0d9d76dddeba09d6a05
@HermesonAlessandro <105324312+HermesonAlessandro@users.noreply.github.com>
2024-11-17 16:47:44 -0300

    Ajustes!
    Ajustes!

--- Sistema_Funcional_de_Bebidas/src/main/java/Visao/Tela_ajuste_administrador.java
+++ Sistema_Funcional_de_Bebidas/src/main/java/Visao/Tela_ajuste_administrador.java
@@ -43,24 +43,29 @@ public class Tela_ajuste_administrador extends javax.swing.JFrame {//Tela para c
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jPanel1 = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
         jScrollPane2 = new javax.swing.JScrollPane();
         jTable2 = new javax.swing.JTable();
-        jButton1 = new javax.swing.JButton();
         jLabel2 = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
+        jButton1 = new javax.swing.JButton();
         jLabel4 = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
         jMenuBar1 = new javax.swing.JMenuBar();
         jMenu1 = new javax.swing.JMenu();
         jMenuItem1 = new javax.swing.JMenuItem();
         jMenu2 = new javax.swing.JMenu();
         jMenuItem2 = new javax.swing.JMenuItem();
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+        setTitle("Tela_ajuste_administrador");
+        setMinimumSize(new java.awt.Dimension(682, 653));
+        getContentPane().setLayout(null);
 
         jLabel1.setFont(new java.awt.Font("Segoe UI", 0, 48)); // NOI18N
         jLabel1.setText("Administradores");
+        getContentPane().add(jLabel1);
+        jLabel1.setBounds(160, 20, 350, 64);
 
         jTable2.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
@@ -101,65 +106,34 @@ public class Tela_ajuste_administrador extends javax.swing.JFrame {//Tela para c
         });
         jScrollPane2.setViewportView(jTable2);
 
+        getContentPane().add(jScrollPane2);
+        jScrollPane2.setBounds(10, 90, 650, 330);
+
+        jLabel2.setText("Para alterar administrador vá para a aba chamada \"Alterar\" em seguida clique em \"Alterar/ADM\"");
+        getContentPane().add(jLabel2);
+        jLabel2.setBounds(70, 440, 530, 16);
+
+        jLabel3.setText("Para excluir administrador vá para a aba chamada \"Excluir\" em seguida clique em \"Excluir/ADM\"");
+        getContentPane().add(jLabel3);
+        jLabel3.setBounds(70, 460, 540, 16);
+
         jButton1.setText("Voltar");
         jButton1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButton1ActionPerformed(evt);
             }
         });
-
-        jLabel2.setText("Para alterar administrador vá para a aba chamada \"Alterar\" em seguida clique em \"Alterar/ADM\"");
-
-        jLabel3.setText("Para excluir administrador vá para a aba chamada \"Excluir\" em seguida clique em \"Excluir/ADM\"");
+        getContentPane().add(jButton1);
+        jButton1.setBounds(280, 490, 80, 40);
 
         jLabel4.setText("jLabel4");
+        getContentPane().add(jLabel4);
+        jLabel4.setBounds(10, 570, 670, 16);
 
-        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
-        jPanel1.setLayout(jPanel1Layout);
-        jPanel1Layout.setHorizontalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGap(161, 161, 161)
-                        .addComponent(jLabel1))
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGap(300, 300, 300)
-                        .addComponent(jButton1)))
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addContainerGap()
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGap(0, 39, Short.MAX_VALUE)
-                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 603, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addGap(34, 34, 34))
-                            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                    .addComponent(jLabel2)
-                                    .addComponent(jLabel3))
-                                .addGap(80, 80, 80))))
-                    .addComponent(jLabel4, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
-        );
-        jPanel1Layout.setVerticalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jLabel1)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 261, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(18, 18, 18)
-                .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 16, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jLabel3)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(jButton1)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 88, Short.MAX_VALUE)
-                .addComponent(jLabel4)
-                .addContainerGap())
-        );
+        jLabel5.setBackground(new java.awt.Color(0, 255, 255));
+        jLabel5.setOpaque(true);
+        getContentPane().add(jLabel5);
+        jLabel5.setBounds(0, 0, 720, 650);
 
         jMenu1.setText("Alterar");
 
@@ -187,18 +161,8 @@ public class Tela_ajuste_administrador extends javax.swing.JFrame {//Tela para c
 
         setJMenuBar(jMenuBar1);
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-        );
-
         pack();
+        setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
     private void jMenuItem1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem1ActionPerformed
@@ -235,20 +199,20 @@ public class Tela_ajuste_administrador extends javax.swing.JFrame {//Tela para c
             }
         }
     }//GEN-LAST:event_jTable2MouseClicked
-    /*
-    Este código busca um administrador selecionado em uma tabela (jTable2). Ele obtém o
-    código do administrador da linha selecionada, usa esse código para buscar o
-    administrador no banco de dados, e exibe uma mensagem com o nome do administrador,
-    caso encontrado. Se nenhum administrador for encontrado, exibe uma mensagem de erro.
-    */
-    
+
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         Tela_inicial_administrador tia = new Tela_inicial_administrador();
         tia.setSaudacao("Login ativo desde:", Sessao.getDataHoraEntrada());
         tia.setVisible(true);
         dispose();
     }//GEN-LAST:event_jButton1ActionPerformed
-    private Administrador AdministradorSelecionado;
+    /*
+    Este código busca um administrador selecionado em uma tabela (jTable2). Ele obtém o
+    código do administrador da linha selecionada, usa esse código para buscar o
+    administrador no banco de dados, e exibe uma mensagem com o nome do administrador,
+    caso encontrado. Se nenhum administrador for encontrado, exibe uma mensagem de erro.
+    */
+        private Administrador AdministradorSelecionado;
     private void ListarAdministrador(){
         AdministradorDAO dao = new AdministradorDAO();
         List<Administrador> administradores = dao.ListarAdministrador();
@@ -287,7 +251,7 @@ public class Tela_ajuste_administrador extends javax.swing.JFrame {//Tela para c
          */
         try {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
+                if ("Windows".equals(info.getName())) {
                     javax.swing.UIManager.setLookAndFeel(info.getClassName());
                     break;
                 }
@@ -317,12 +281,12 @@ public class Tela_ajuste_administrador extends javax.swing.JFrame {//Tela para c
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
     private javax.swing.JMenu jMenu1;
     private javax.swing.JMenu jMenu2;
     private javax.swing.JMenuBar jMenuBar1;
     private javax.swing.JMenuItem jMenuItem1;
     private javax.swing.JMenuItem jMenuItem2;
-    private javax.swing.JPanel jPanel1;
     private javax.swing.JScrollPane jScrollPane2;
     private javax.swing.JTable jTable2;
     // End of variables declaration//GEN-END:variables
