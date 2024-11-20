4b4beffb4718b5e12d0bd49c86546b4dd5b599b7
@HermesonAlessandro <105324312+HermesonAlessandro@users.noreply.github.com>
2024-11-20 11:00:07 -0300

    Ajuste!
    Ajuste!

--- Sistema_Funcional_de_Bebidas/src/main/java/Visao/Tela_cadastrar_bebida.java
+++ Sistema_Funcional_de_Bebidas/src/main/java/Visao/Tela_cadastrar_bebida.java
@@ -45,7 +45,6 @@ public class Tela_cadastrar_bebida extends javax.swing.JFrame {//Tela de Cadastr
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jPanel1 = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
         jTextField1 = new javax.swing.JTextField();
@@ -64,39 +63,79 @@ public class Tela_cadastrar_bebida extends javax.swing.JFrame {//Tela de Cadastr
         jButton1 = new javax.swing.JButton();
         jButton2 = new javax.swing.JButton();
         jLabel9 = new javax.swing.JLabel();
+        jLabel10 = new javax.swing.JLabel();
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+        setTitle("Tela_cadastrar_bebida");
+        setMinimumSize(new java.awt.Dimension(737, 603));
+        getContentPane().setLayout(null);
 
         jLabel1.setFont(new java.awt.Font("Segoe UI", 0, 48)); // NOI18N
         jLabel1.setText("Cadastrar Bebida");
+        getContentPane().add(jLabel1);
+        jLabel1.setBounds(190, 30, 360, 64);
 
         jLabel2.setText("Cod");
+        getContentPane().add(jLabel2);
+        jLabel2.setBounds(160, 120, 30, 16);
 
         jTextField1.setEditable(false);
+        getContentPane().add(jTextField1);
+        jTextField1.setBounds(200, 120, 390, 22);
 
         jLabel3.setText("Descriçao");
+        getContentPane().add(jLabel3);
+        jLabel3.setBounds(160, 160, 60, 16);
+        getContentPane().add(jTextField2);
+        jTextField2.setBounds(220, 160, 370, 22);
 
         jLabel4.setText("Marca");
+        getContentPane().add(jLabel4);
+        jLabel4.setBounds(160, 200, 40, 16);
 
         jTextField3.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jTextField3ActionPerformed(evt);
             }
         });
+        getContentPane().add(jTextField3);
+        jTextField3.setBounds(210, 200, 380, 22);
 
         jLabel5.setText("Gp_mercadoria");
+        getContentPane().add(jLabel5);
+        jLabel5.setBounds(160, 240, 90, 16);
 
         jComboBox1.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Bebidas Fermentadas", "Bebidas Destiladas", "Licores e Outras Bebidas", "Coquetéis e Drinks" }));
+        getContentPane().add(jComboBox1);
+        jComboBox1.setBounds(260, 240, 162, 22);
 
         jLabel6.setText("T_de_item");
+        getContentPane().add(jLabel6);
+        jLabel6.setBounds(160, 280, 70, 16);
 
         jComboBox2.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Direto ao Cliente", "Revenda" }));
+        getContentPane().add(jComboBox2);
+        jComboBox2.setBounds(230, 280, 130, 22);
 
         jLabel7.setText("Q_estoque");
+        getContentPane().add(jLabel7);
+        jLabel7.setBounds(160, 320, 70, 16);
+
+        jTextField4.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jTextField4ActionPerformed(evt);
+            }
+        });
+        getContentPane().add(jTextField4);
+        jTextField4.setBounds(240, 320, 350, 22);
 
         jLabel8.setText("V_unitário");
+        getContentPane().add(jLabel8);
+        jLabel8.setBounds(160, 360, 70, 16);
 
         jTextField5.setText("R$ ");
+        getContentPane().add(jTextField5);
+        jTextField5.setBounds(230, 360, 360, 22);
 
         jButton1.setText("Cadastrar");
         jButton1.addActionListener(new java.awt.event.ActionListener() {
@@ -104,6 +143,8 @@ public class Tela_cadastrar_bebida extends javax.swing.JFrame {//Tela de Cadastr
                 jButton1ActionPerformed(evt);
             }
         });
+        getContentPane().add(jButton1);
+        jButton1.setBounds(190, 450, 90, 40);
 
         jButton2.setText("Voltar");
         jButton2.addActionListener(new java.awt.event.ActionListener() {
@@ -111,131 +152,34 @@ public class Tela_cadastrar_bebida extends javax.swing.JFrame {//Tela de Cadastr
                 jButton2ActionPerformed(evt);
             }
         });
+        getContentPane().add(jButton2);
+        jButton2.setBounds(430, 450, 90, 40);
 
         jLabel9.setText("jLabel9");
+        getContentPane().add(jLabel9);
+        jLabel9.setBounds(10, 540, 720, 16);
 
-        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
-        jPanel1.setLayout(jPanel1Layout);
-        jPanel1Layout.setHorizontalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(jButton1)
-                .addGap(133, 133, 133)
-                .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 80, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(235, 235, 235))
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGap(80, 80, 80)
-                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                            .addGroup(jPanel1Layout.createSequentialGroup()
-                                .addComponent(jLabel4, javax.swing.GroupLayout.PREFERRED_SIZE, 37, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(jTextField3))
-                            .addGroup(jPanel1Layout.createSequentialGroup()
-                                .addComponent(jLabel3)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(jTextField2))
-                            .addGroup(jPanel1Layout.createSequentialGroup()
-                                .addComponent(jLabel2)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 523, javax.swing.GroupLayout.PREFERRED_SIZE))
-                            .addGroup(jPanel1Layout.createSequentialGroup()
-                                .addComponent(jLabel5)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                            .addGroup(jPanel1Layout.createSequentialGroup()
-                                .addComponent(jLabel6)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(jComboBox2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                            .addGroup(jPanel1Layout.createSequentialGroup()
-                                .addComponent(jLabel7)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(jTextField4))
-                            .addGroup(jPanel1Layout.createSequentialGroup()
-                                .addComponent(jLabel8, javax.swing.GroupLayout.PREFERRED_SIZE, 56, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(jTextField5))))
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGap(189, 189, 189)
-                        .addComponent(jLabel1)))
-                .addContainerGap(106, Short.MAX_VALUE))
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jLabel9, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-        );
-        jPanel1Layout.setVerticalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(37, 37, 37)
-                .addComponent(jLabel1)
-                .addGap(47, 47, 47)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel2)
-                    .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(18, 18, 18)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jLabel3)
-                    .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(18, 18, 18)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel4)
-                    .addComponent(jTextField3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(21, 21, 21)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jLabel5)
-                    .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel6)
-                    .addComponent(jComboBox2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(18, 18, 18)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel7)
-                    .addComponent(jTextField4, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(18, 18, 18)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jLabel8)
-                    .addComponent(jTextField5, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(52, 52, 52)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jButton1)
-                    .addComponent(jButton2))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 70, Short.MAX_VALUE)
-                .addComponent(jLabel9)
-                .addContainerGap())
-        );
-
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-        );
+        jLabel10.setBackground(new java.awt.Color(0, 255, 255));
+        jLabel10.setOpaque(true);
+        getContentPane().add(jLabel10);
+        jLabel10.setBounds(0, 0, 750, 620);
 
         pack();
+        setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
     private void jTextField3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField3ActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_jTextField3ActionPerformed
 
-    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-        Tela_inicial_secretaria tis = new Tela_inicial_secretaria();
-        tis.setSaudacao("Login ativo desde:", Sessao.getDataHoraEntrada());
-        tis.setVisible(true);
-        dispose();
-    }//GEN-LAST:event_jButton2ActionPerformed
+    private void jTextField4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField4ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jTextField4ActionPerformed
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         if(jTextField2.getText().isEmpty() || jTextField3.getText().isEmpty() ||
-           jComboBox1.getSelectedItem() == null || jComboBox2.getSelectedItem() == null ||
-           jTextField4.getText().isEmpty() || jTextField5.getText().isEmpty()){
+            jComboBox1.getSelectedItem() == null || jComboBox2.getSelectedItem() == null ||
+            jTextField4.getText().isEmpty() || jTextField5.getText().isEmpty()){
             JOptionPane.showMessageDialog(null, "Campos vazios, por favor preencher todos os campos!");
         }else{
             Bebida bebida = new Bebida();
@@ -244,12 +188,12 @@ public class Tela_cadastrar_bebida extends javax.swing.JFrame {//Tela de Cadastr
             bebida.setGp_mercadoria(jComboBox1.getSelectedItem().toString());
             bebida.setT_do_item(jComboBox2.getSelectedItem().toString());
             bebida.setQ_estoque(Integer.parseInt(jTextField4.getText()));
-            
+
             String valorUnitarioStr = jTextField5.getText().replace("R$", "").trim().replace(",", ".");
             NumberFormat nf = NumberFormat.getInstance(Locale.US);
             //Capturar e formatar um valor monetário, removendo o símbolo de moeda e ajustando o formato de decimal.
             //Criar um formatador numérico que segue as convenções dos Estados Unidos, permitindo manipulação e conversão consistentes de números.
-            
+
             try{
                 Number number = nf.parse(valorUnitarioStr);
                 bebida.setV_unitario(number.doubleValue());
@@ -262,15 +206,15 @@ public class Tela_cadastrar_bebida extends javax.swing.JFrame {//Tela de Cadastr
                 JOptionPane.showMessageDialog(null, "Erro ao formatar o valor unitario da bebida: " +e.getMessage());
                 return;
             }
-            
+
             String CodigodeBarras = GerarCodigodeBarras();
             bebida.setCod_de_barras(CodigodeBarras);
             //Usado para gerar um código de barras.
-            
+
             try{
                 BebidaDAO dao = new BebidaDAO();
                 if(dao.DescricaoExiste(bebida.getDescricao())){
-                     JOptionPane.showMessageDialog(null, "Descricao da bebida ja existe. Tente novamente!");
+                    JOptionPane.showMessageDialog(null, "Descricao da bebida ja existe. Tente novamente!");
                 }else{
                     dao.CadastrarBebida(bebida);
                     JOptionPane.showMessageDialog(null, "Bebida cadastrada com sucesso! codigo de barras gerado: " + CodigodeBarras);
@@ -280,6 +224,13 @@ public class Tela_cadastrar_bebida extends javax.swing.JFrame {//Tela de Cadastr
             }
         }
     }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        Tela_inicial_secretaria tis = new Tela_inicial_secretaria();
+        tis.setSaudacao("Login ativo desde:", Sessao.getDataHoraEntrada());
+        tis.setVisible(true);
+        dispose();
+    }//GEN-LAST:event_jButton2ActionPerformed
     /*
     Esse método verifica se todos os campos estão preenchidos.
     Cria um objeto Bebida com os valores fornecidos.
@@ -314,7 +265,7 @@ public class Tela_cadastrar_bebida extends javax.swing.JFrame {//Tela de Cadastr
          */
         try {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
+                if ("Windows".equals(info.getName())) {
                     javax.swing.UIManager.setLookAndFeel(info.getClassName());
                     break;
                 }
@@ -344,6 +295,7 @@ public class Tela_cadastrar_bebida extends javax.swing.JFrame {//Tela de Cadastr
     private javax.swing.JComboBox<String> jComboBox1;
     private javax.swing.JComboBox<String> jComboBox2;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel10;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
@@ -352,7 +304,6 @@ public class Tela_cadastrar_bebida extends javax.swing.JFrame {//Tela de Cadastr
     private javax.swing.JLabel jLabel7;
     private javax.swing.JLabel jLabel8;
     private javax.swing.JLabel jLabel9;
-    private javax.swing.JPanel jPanel1;
     private javax.swing.JTextField jTextField1;
     private javax.swing.JTextField jTextField2;
     private javax.swing.JTextField jTextField3;
